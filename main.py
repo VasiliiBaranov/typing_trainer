71a0ffaa1a2da0db81b5aa87b2a174ab14e02709
VasiliyBaranov <bargl1@ya.ru>
2023-11-05 23:17:29 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,220 @@
+import sys  # sys нужен для передачи argv в QApplication
+
+from PyQt5 import QtWidgets, QtCore
+# from PyQt5.QtGui import QFont, QShowEvent
+import PyQt5.QtGui as QtGui
+
+import texts
+
+import skelet  # Это наш конвертированный файл дизайна
+
+
+class App(QtWidgets.QMainWindow, skelet.Ui_MainWindow):
+    def __init__(self):
+        '''
+        create fields of object 🤯
+        не знаю что тут можно написать
+        '''
+        # Это здесь нужно для доступа к переменным, методам
+        # и т.д. в файле design.py
+        super().__init__()
+
+        # Fields
+        self.output_text = str()
+        # self.start_output_length = len(self.output_text)
+        self.start_output_length = 0
+        self.lang = str()
+        self.percent = 0
+        self.right_letters = 0
+
+        # Set Title
+        self.setupUi(self)  # Это нужно для инициализации нашего дизайна
+        self.setWindowTitle("Тренажер для печати")
+        # self.setGeometry(1, 1, 2500, 1000)
+
+        # Input - Output
+        # self.input_string.setGeometry(0, 0, 500, 300)
+        self.input_string.textChanged.connect(self.text_changed)
+
+        # Ready Steady Go Lable
+        # self.start_lable_number
+
+        # Progress Bar
+        self.progress_bar.setFont(QtGui.QFont("Times New Roman", 10, 10, False))
+        self.progress_bar.setValue(self.percent)
+
+        # self.verticalLayout.setGeometry(QRect(200,200,1000,1000))
+        self.pause_button.clicked.connect(self.start)
+
+        # Restart Button
+        self.restart_button.hide()
+        self.restart_button.setText("Перезапустить")
+        self.restart_button.clicked.connect(self.restart)
+
+        # Choice language
+        self.choice_language.addItem("eng")
+        self.choice_language.addItem("rus")
+        # self.choice_language.activated.connect(self.start)
+
+        # Button_start
+        self.pause_button.setText("Поехали!")
+
+        # Property set checkbox:
+        self.is_capital_in_text = False
+        self.is_digits_in_text = False
+        self.is_other_in_text = False
+
+        self.digits_check.clicked.connect(self.update_digits)
+        self.capital_check.clicked.connect(self.update_capital)
+        self.other_check.clicked.connect(self.update_other)
+
+        # Timer
+        self.timer = QtCore.QTimer()
+        self.time = 0
+        self.timer.setInterval(500)
+        self.timer.timeout.connect(self.update_time)
+
+        # self.timer2 = QtCore.QTimer() TODO this byaka
+        # self.timer2.singleShot(5000, self.aboba)
+        # Timer to start game
+
+        # Time and Speed Lables
+
+        self.time_lable.setText(f"Время: -")
+        self.speed_label.setText(f"Скорость печати: - сим./мин.")
+
+    def text_changed(self):
+        '''if text changed, we check if new letter is right.
+        If it is, then we delete this letter from the text in both strings.
+        If it is not, then we delete this letter only from input string
+        Of course, we make some difficult calculations with counting how many right letters we typed with this moment
+
+        '''
+        if self.output_string.text() == "":
+            return
+        if self.input_string.text() == "":
+            return
+        # print(self.output_string.text(), "aboba", self.input_string.text())
+        self.input_string.disconnect()
+        if self.output_text[0] == self.input_string.text()[-1]:
+            self.output_text = self.output_text[1:]
+            self.output_string.setText(self.output_text[:min(len(self.output_text), 25)])
+            if self.input_string.text()[-1] == ' ':
+                self.input_string.setText("")
+            self.percent += 1 / (self.text.number_of_symbols) * 100
+            self.right_letters += 1
+        else:
+            self.input_string.setText(self.input_string.text()[:-1])
+
+        if self.output_string.text() == "":
+            print("The end")
+            exit(0)
+        self.input_string.textChanged.connect(self.text_changed)
+        self.update_progress_bar()
+
+    def start(self):
+        '''start game - generate and show new text, start timer'''
+        self.timer.start()
+
+        self.text = texts.Text(3, 15,
+                               self.is_capital_in_text, self.is_digits_in_text, self.is_other_in_text)
+        print(self.text)
+        # text = "qwer gkIDk 1234 fkldi Lqwer gkIDk 1234 fkldi L"
+        self.output_text = str(self.text)
+        # self.start_output_length = self.text.number_of_symbols
+        self.output_string.setText(self.output_text[:25])
+
+        self.pause_button.setText("Пауза")
+        self.pause_button.disconnect()
+        self.pause_button.clicked.connect(self.pause)
+        self.restart_button.show()
+        self.choice_language.hide()
+        self.lang = self.choice_language.currentText()
+        # print(self.lang)
+        self.start_output_length = len(self.output_string.text())
+        # self.input_string.setUpdatesEnabled(True)
+
+    def update_time(self):
+        '''technical method. He is... update time, and speed on the screen'''
+        self.time += 0.5
+        # self.time_lable.setText(str(self.time))
+        self.time_lable.setText(f"Время: {self.time}")
+        # self.speed_label.setText(str(self.right_letters / self.time * 60))
+        self.speed_label.setText(f"Скорость печати: {self.right_letters / self.time * 60} сим./мин.")
+
+    def update_progress_bar(self):
+        '''technical method. He is ... update progress bar'''
+        self.progress_bar.setValue(int(self.percent))
+
+    def pause(self):
+        '''pause game - stop time, lock the opportunity to type letters in string'''
+        self.pause_button.setText("Продолжить")
+        self.pause_button.disconnect()
+        self.pause_button.clicked.connect(self.cont)
+        self.timer.stop()
+        self.input_string.setReadOnly(True)
+
+    def restart(self):
+        '''Restart the game - reset all parameters, generate new text'''
+        self.input_string.setReadOnly(False)
+        self.input_string.setText("")
+        self.right_letters = 0
+        self.percent = 0
+        self.time = 0
+        self.timer.start()
+        text = texts.Text(3, 15,
+                          self.is_capital_in_text, self.is_digits_in_text, self.is_other_in_text)
+        self.output_text = str(text)
+
+        self.output_string.setText(self.output_text[:25])
+
+        self.pause_button.setText("Пауза")
+        self.pause_button.disconnect()
+        self.pause_button.clicked.connect(self.pause)
+        self.restart_button.show()
+        self.choice_language.hide()
+        self.lang = self.choice_language.currentText()
+        self.update_progress_bar()
+
+    def cont(self):  # Continue typing
+        '''continue game: continue timer, allow to type'''
+        self.pause_button.setText("Пауза")
+        self.pause_button.disconnect()
+        self.pause_button.clicked.connect(self.pause)
+        self.timer.start()
+        self.input_string.setReadOnly(False)
+
+    def update_digits(self):
+        '''technical method. Life is life
+        if we want to change some characteristic of our text
+        '''
+        self.is_digits_in_text = not self.is_digits_in_text
+
+    def update_capital(self):
+        '''technical method. Life is life
+            if we want to change some characteristic of our text
+            '''
+        self.is_capital_in_text = not self.is_capital_in_text
+
+    def update_other(self):
+        '''technical method. Life is life
+            if we want to change some characteristic of our text
+            '''
+        self.is_other_in_text = not self.is_other_in_text
+
+
+def main():
+    '''the most difficult part of my project is function main()
+    In this function I create the app
+    create the window
+    show the window
+    and execute the app
+    '''
+    app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
+    window = App()  # Создаём объект класса ExampleApp
+    window.show()  # Показываем окно
+    app.exec_()  # и запускаем приложение
+
+
+if __name__ == '__main__':
+    main()
---
+++ texts.py
@@ -0,0 +1,70 @@
+import random
+from english_words import english_words_set
+
+list_of_words = list(english_words_set)
+
+# list_of_words = ['мама', 'папа']
+length_of_dict = len(list_of_words)
+lst_of_words = []
+
+for i in list_of_words:
+    lst_of_words.append(i.lower())
+
+cap_list_w = []
+for i in list_of_words:
+    cap_list_w.append(i.capitalize())
+    cap_list_w.append(i)
+'''
+object with generated text
+'''
+
+
+class Text(object):
+
+    def __init__(self, min_length, max_length, capital_e, digits_e, other_e):
+        self.length = random.randint(min_length, max_length)
+        self.data = []
+
+        self._digits_col = 0
+        self._other_col = 0
+        self.number_of_symbols = 0
+
+        if digits_e:
+            self._digits_col = random.randint(0, int(self.length / 10))
+
+        if other_e:
+            self._other_col = random.randint(0, int(self.length / 15))
+
+        # add words in list
+        self.tmp_list = lst_of_words
+        if capital_e:
+            self.tmp_list = cap_list_w
+
+        if self._digits_col:
+            for _ in range(self._digits_col):
+                self.data.append(str(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])))
+
+        if self._other_col:
+            for _ in range(self._other_col):
+                self.data.append(random.choice(
+                    ["!", "@", "#", "$", "%", "^", "*", "(", ")", "_", "+", "{", "}", "[", "]", ":", "\"", ";", "'",
+                     "<",
+                     ">", ",", ".", ">", "?"]))
+
+        for _ in range(self.length - self._digits_col - self._other_col):
+            self.data.append(random.choice(self.tmp_list))
+
+        # Make string from data
+        random.shuffle(self.data)
+        self.string = " ".join(self.data)
+        self.number_of_symbols = len(self.string)
+
+    def __str__(self):
+        return self.string
+
+    def __len__(self):
+        return self.length
+
+# t = Text(3, 15, False, False, False)
+# print(t)
+# print(t.number_of_symbols)
